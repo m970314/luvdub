62aad0b335cdf8effb35c00d9eb8afd9be94eb1e
jojaewon <m970314@naver.com>
2023-11-08 22:22:07 +0900

    [조재원] Login 화면 Ui 구현(진행중)

--- app/src/main/java/com/example/luvdub/Login/LoginFragment.kt
+++ app/src/main/java/com/example/luvdub/Login/LoginFragment.kt
@@ -1,47 +1,57 @@
 package com.example.luvdub.Login
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.Fragment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class LoginFragment : Fragment() {
+class LoginFragment {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                LoginScreen()
+    @Composable
+    fun LoginScreen() {
+        // Login 화면 그리기
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.padding(30.dp))
+            Text(text = "쉽고 빠르고 새로운\n우리만의 연락방법", fontSize = 36.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.padding(14.dp))
+
+            Text(
+                // 하나의 text에서 다른 폰트스타일 적용
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontSize = 20.sp)) {
+                        append("이제 더 이상 연락으로 다투지말고\n")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("럽둡(Luv-Dub)")
+                        }
+                        append("으로 시그널을 보내요")
+                    }
+                }
+            )
+
+            Button(onClick = { /*TODO*/ }) {
+                Spacer(modifier = Modifier
+                    .clip(RoundedCornerShape(60.dp))
+                    .then(Modifier.height(56.dp))
+                )
             }
         }
     }
-}
 
-@Composable
-fun LoginScreen() {
-    Column(modifier = Modifier.padding(24.dp)) {
-        Text(text = "쉽고 빠르고 새로운\n우리만의 연락방법")
-        Text(text = "쉽고 빠르고 123")
-        // 여기에 로그인 UI 구성 요소를 추가합니다.
-    }
-}
-
-@Preview
-@Composable
-fun PreviewLoginScreen() {
-    LoginScreen()
 }
