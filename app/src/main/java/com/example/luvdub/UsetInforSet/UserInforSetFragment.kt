557e1ce63a5baf044aad7f3c6fcc1f9b849a82a9
jojaewon <m970314@naver.com>
2023-11-13 22:06:43 +0900

    [조재원] 유저 프로필 설정 Step화면 구현

--- app/src/main/java/com/example/luvdub/UsetInforSet/UserInforSetFragment.kt
+++ app/src/main/java/com/example/luvdub/UsetInforSet/UserInforSetFragment.kt
@@ -1,25 +1,60 @@
 package com.example.luvdub.UsetInforSet
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.luvdub.R
+import com.example.luvdub.data_store.IntPreferencesKey
+import com.example.luvdub.data_store.LuvdubDataStoreManager
 import com.example.luvdub.ui.theme.Black
 
 class UserInforSetFragment {
     @Composable
     fun UserInforSetScrren() {
-        Column(modifier = Modifier.fillMaxSize()) {
+        val context = LocalContext.current
+        val currentStep by LuvdubDataStoreManager.getLuvdubIntPreferencesStore(
+            context,
+            IntPreferencesKey.LUV_CURRENT_TAB_INT
+        ).collectAsState(initial = 0)
+
+        val MAX_STEP = 5
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
             CustomTitleBar()
+
+            when (currentStep) {
+                // 스텝별 화면 설정
+                1 -> Step1Content()
+                2 -> Step2Content()
+                3 -> Step3Content()
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(onClick = {
+                // 다음 스텝으로 이동
+
+            }) {
+                Text("다음으로")
+            }
         }
     }
 
@@ -35,5 +70,23 @@ class UserInforSetFragment {
                 modifier = Modifier.size(24.dp))
             Text("Custom Title", modifier = Modifier.padding(16.dp), fontWeight = FontWeight.Bold)
         }
+    }
+
+    @Composable
+    fun Step1Content() {
+        // Step 1의 UI 구성
+        Text("Step 1 Content")
+    }
+
+    @Composable
+    fun Step2Content() {
+        // Step 2의 UI 구성
+        Text("Step 2 Content")
+    }
+
+    @Composable
+    fun Step3Content() {
+        // Step 3의 UI 구성
+        Text("Step 3 Content")
     }
 }
---
+++ app/src/main/java/com/example/luvdub/UsetInforSet/UserInforSetViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.luvdub.UsetInforSet
+
+class UserInforSetViewModel {
+}
